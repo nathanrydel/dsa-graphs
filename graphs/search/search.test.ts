fd5db735d5bf511e169bd7f243e1c50badec6080
Nathan Rydel <nathanrydel@gmail.com>
2024-02-29 06:21:54 -0700

    updated testing rigor

--- graphs/search/search.test.ts
+++ graphs/search/search.test.ts
@@ -3,26 +3,15 @@ import { GraphStr } from "../common/graph";
 import { bfs, iDfs, rDfs } from "./search";
 
 describe("DFS", function () {
-  it("returns array of nodes using rDfs", function () {
-    //    d -- b -- a -- c
-    //     \------------/
-
+  it.each([
+    ["iDFS", iDfs],
+    ["rDFS", rDfs],
+  ])("DFS does a-b-d-c or a-c-d-b (%s)", function (name, fn) {
     const graph = new GraphStr();
     const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
     graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
     const rez = rDfs(a).join("-");
-    expect([ "a-c-b-d", "a-b-d-c", "a-c-d-b" ]).toContain(rez);
-  });
-
-  it("returns array of nodes using iDfs", function () {
-    //    d -- b -- a -- c
-    //     \------------/
-
-    const graph = new GraphStr();
-    const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
-    graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
-    const rez = iDfs(a).join("-");
-    expect([ "a-c-b-d", "a-b-d-c", "a-c-d-b" ]).toContain(rez);
+    expect(["a-b-d-c", "a-c-d-b"]).toContain(rez);
   });
 });
 
@@ -35,6 +24,6 @@ describe("BFS", function () {
     const [a, b, c, d] = graph.addFromVals(["a", "b", "c", "d"]);
     graph.addEdges([[d, b], [b, a], [a, c], [c, d]]);
     const rez = bfs(a).join("-");
-    expect([ "a-b-c-d", "a-c-b-d" ]).toContain(rez);
+    expect(["a-b-c-d", "a-c-b-d"]).toContain(rez);
   });
 });
