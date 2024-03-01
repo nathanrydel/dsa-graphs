e94b00147a75224c1094f54246084807fc9e84f6
Eric Huie <erichuie97@gmail.com>
2024-02-29 16:27:42 -0800

    added iterative DFS for graphs

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,18 +1,40 @@
 import { GNodeStr } from "../graph/graph";
-
+import { Stack } from "../common/stack";
 /** Return array of nodes, in DFS order (recursive version)  */
 
 function rDfs(
   start: GNodeStr,
   result: string[] = [],
   visited = new Set([start])): string[] {
+
+    // if(start === null) return null;
+
+    for(const adj of start.adjacent){
+      rDfs(adj)
+    }
+
   return ["todo"];
 }
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: GNodeStr): string[] {
-  return ["todo"];
+  const stack = new Stack<GNodeStr>([start]);
+  const visited = new Set<GNodeStr>([start]);
+  const dfsStrs: string[] = [];
+
+  while(!stack.isEmpty()){
+    const currNode = stack.pop();
+    dfsStrs.push(currNode.value);
+    for(const adj of currNode.adjacent){
+      if(!visited.has(adj)){
+        visited.add(adj);
+        stack.push(adj);
+      }
+    }
+  }
+
+  return dfsStrs;
 }
 
 /** Return array of nodes, in BFS order (iterative version)  */
